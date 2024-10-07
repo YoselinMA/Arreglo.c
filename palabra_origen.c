3d2b10817cd97f72e2913d0e96d7653fe8667830
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-10-06 18:24:26 -0600

    Create palabra_origen.c

---
+++ palabra_origen.c
@@ -0,0 +1,11 @@
+#include <string.h>
+#include <stdio.h>
+
+int main (){
+    int len;
+    char origen[] = "Origen";
+    char destino [7];
+    strcpy (destino, origen);
+    printf("destino: %s ", destino);
+    
+}
